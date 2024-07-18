347547ee34b5f980b154b0b3bfc8b6ae24f12d68
alex aku <vixndotcom@gmail.com>
2024-07-18 05:34:04 +0100

    arrow

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
      <section className={`${styles.flexStart} `} >
        <div className='mt-20'>
  
-       <h1 className=' mt-20 shadow-text text-start text-white' >
+       <h1 className=' mt-20 text-shadow text-start text-white' >
          <div className='mb-5 galgo tracking-wide text-7xl'>Stimulating the  <span className='purple'>Mind</span> </div>
          <div className='mb-5 galgo tracking-wide text-7xl '>and <span className='purple'> Unraveling difficult</span> </div>
          <div className='mb-5 galgo tracking-wide text-7xl'>Thought Processes </div>
@@ -28,8 +28,8 @@ const Hero = () => {
 
       <button className=' w-[320px]  arrow  rounded-[12px] bg-arrow mt-8 text-white' >
        <Link to ='/about' className='flex flex-row text-center md:mt-4  w-[300px] h-[140px]'>
-       <p className='text-[24px] mx-8  mt-5 neue-thin  text-shadow'>Book a Service</p>
-        <img src={arrow} alt="" className='object contain w-[54px] h-[51px] mx-4 mt-4'/>
+       <p className='text-[24px] mx-8  mt-8 neue-thin  text-shadow'>Book a Service</p>
+        <img src={arrow} alt="" className='object contain w-[54.73] h-[53.75] mx-4 mt-5'/>
        </Link>    
           </button> 
 
