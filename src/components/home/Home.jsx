e169febe29f4a80324047bfeb8cfbc79303754d8
alex aku <vixndotcom@gmail.com>
2024-07-18 09:46:24 +0100

    hero

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,12 +6,12 @@ import SecoPara from './SecoPara'
 
 const Home = () => {
   return (
-    <div className='w-full overflow-hidden' >
+    <div className='w-full overflow-hidden bg-purple' >
     <div className='bg-hero' >
       <Welcome />
     </div>
     
-    <div className='bg-purple '>
+    <div className=' '>
     {/* <div className='bg-curve p-2 z-[999]'>
 
     </div> */}
