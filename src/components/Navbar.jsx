9dc6932ef7c9bad6c84a77aa1af4c3042d995adb
alex aku <vixndotcom@gmail.com>
2024-07-18 10:25:04 +0100

    nav icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
          <img
             src={toggle ? close : menu}
             alt="menu"
-            className='w-[90px] h-[70px] object-contain mt-1'
+            className='w-[80px] h-[50px] object-contain mt-1'
             onClick={() => setToggle(!toggle)}
           />
          </div>
