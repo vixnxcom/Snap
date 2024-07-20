4469ce2855a18f2e0e57298b2a80ac7503de71f2
alex aku <vixndotcom@gmail.com>
2024-07-20 17:19:13 +0100

    box

--- src/components/about/Box.jsx
+++ src/components/about/Box.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 export const Box = () => {
   return (
     // <section className=''>
-          <div className='scroll-container'>
-            <div className='scroll-box sroll-top  '>Unlock</div>
-            <div className='scroll-box sroll-top '>Simplify</div>
-            <div className='scroll-box sroll-top '>Transform</div>
-            <div className='scroll-box sroll-top '>Navigate</div>
-            <div className='scroll-box sroll-top '>Perfect</div>
+          <div className='scroll-container font-medium'>
+            <div className='scroll-box sroll-top  '>TRANSPARENCY</div>
+            <div className='scroll-box sroll-top '>OPEN COMMUNICATION</div>
+            {/* <div className='scroll-box sroll-top '>Transform</div> */}
+            <div className='scroll-box sroll-top '>ACCOUNTABLE</div>
+            <div className='scroll-box sroll-top '>DIVERSITY AND INCLUSION</div>
           {/* <h1 className='text-black text-[24px] font-bold '>your <span className='text-[20px]'>PLAB</span></h1> */}
           </div>
 
@@ -17,3 +17,4 @@ export const Box = () => {
         
   )
 }
+
