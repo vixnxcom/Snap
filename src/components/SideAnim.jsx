34da48f8e2c83d78302a27066ca19706f68582f1
alex aku <vixndotcom@gmail.com>
2024-08-04 09:19:25 +0100

    threshold

--- src/components/SideAnim.jsx
+++ src/components/SideAnim.jsx
@@ -21,7 +21,7 @@ const SideAnim = ({ children }) => {
           }
         });
       },
-      { threshold: 0.1 } // Trigger when 10% of the element is visible
+      { threshold: 0.2 } // Trigger when 10% of the element is visible
     );
 
     if (element) {
