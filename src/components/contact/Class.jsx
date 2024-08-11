220f014be9abe68d7e0225e7e8cfd8c1643af5a5
alex aku <vixndotcom@gmail.com>
2024-08-11 14:33:03 +0100

    bootcamp

--- src/components/contact/Class.jsx
+++ src/components/contact/Class.jsx
@@ -10,11 +10,7 @@ const Class = () => {
 
     <div className='p-2 flex flex-row gap-8 mx-auto'> 
     {/* <p className='neue-medium text-[24px] mt-1 hover:tracking-widest'>Buy Tickets</p> */}
-    <a href="A 3weeks Online Bootcamp
-August 19, 2024, 6:00pm – September 6, 2024, 7:00pm · 
-Time zone: Africa/Lagos
-Google Meet joining info
-Video call link: https://meet.google.com/vtb-mahr-osq" target="_blank" className='neue-medium text-[24px] mt-1 hover:tracking-widest' >
+    <a href=" https://meet.google.com/vtb-mahr-osq" target="_blank" rel="noopener noreferrer"  className='neue-medium text-[24px] mt-1 hover:tracking-widest' >
 
         Join class </a>
     <img src={arroww} alt="" className='w-[43.43px] h-[41.55px] ' />
