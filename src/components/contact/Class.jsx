3b181eb1fd7ab55e703fb9bacaca4c489f80311d
alex aku <vixndotcom@gmail.com>
2024-08-11 14:25:51 +0100

    class

--- src/components/contact/Class.jsx
+++ src/components/contact/Class.jsx
@@ -10,8 +10,12 @@ const Class = () => {
 
     <div className='p-2 flex flex-row gap-8 mx-auto'> 
     {/* <p className='neue-medium text-[24px] mt-1 hover:tracking-widest'>Buy Tickets</p> */}
-    <a href="https://shows.ng/event/A-3-week-online-Bootcamp-training-267" 
-     target="_blank" rel="noopener noreferrer" className='neue-medium text-[24px] mt-1 hover:tracking-widest'>
+    <a href="A 3weeks Online Bootcamp
+August 19, 2024, 6:00pm – September 6, 2024, 7:00pm · 
+Time zone: Africa/Lagos
+Google Meet joining info
+Video call link: https://meet.google.com/vtb-mahr-osq" target="_blank" className='neue-medium text-[24px] mt-1 hover:tracking-widest' >
+
         Join class </a>
     <img src={arroww} alt="" className='w-[43.43px] h-[41.55px] ' />
     </div> 
--- src/components/contact/Steps.jsx
+++ src/components/contact/Steps.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import styles from '../../style'
+import { arrowhite } from '../../assets';
 
 const Steps = () => {
+  const emailAddress = 'championsglobalconsulting@gmail.com';
+  
+  
   return (
    <section>
     <div className='mt-5 p-4'>
@@ -16,10 +20,21 @@ const Steps = () => {
      </div> 
   <p className='neue-medium text-[28px] text-white tracking-wide mt-2 mx-4 ' > Purchase a ticket</p>
      </div>
-
+{/* email///////////////////////////////////////// */}
    <div className='flex flex-row mt-2'>
     <div className={`w-[48px] h-[48px] rounded-full ${styles.flexCenter} mt-2 text-white bg-black`}>  
      <h1 className='text-white galgo text-[24px]  tracking-wide'> 02. </h1>
+     </div> 
+     <div className='flex flex-col '>
+  <p className='neue-medium text-[28px] text-white tracking-wide mt-2 mx-4 ' > Email your ticket receipt for <br />confirmation to:</p>
+
+  <a href={`mailto:${emailAddress}` } className='text-white text-[20px] neue-thin mt-2 p-1'>{emailAddress}<img src={arrowhite} alt="" className='w-[36px] mt-2 ' /> </a>
+     </div>
+     </div>
+     {/* card 3/////////////////////////////////// */}
+   <div className='flex flex-row mt-2'>
+    <div className={`w-[48px] h-[48px] rounded-full ${styles.flexCenter} mt-2 text-white bg-black`}>  
+     <h1 className='text-white galgo text-[24px]  tracking-wide'> 03. </h1>
      </div> 
   <p className='neue-medium text-[28px] text-white tracking-wide mt-2 mx-4 ' > Join our class</p>
      </div>
