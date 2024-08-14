e785ea897c3abb2e3e110abbd36e9d14089b3904
alex aku <vixndotcom@gmail.com>
2024-08-14 15:09:14 +0100

    space

--- src/components/home/Bootcamp.jsx
+++ src/components/home/Bootcamp.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 
 const Bootcamp = () => {
   return (
-   <section className='mt-10 mb-10 mb-5 px-18 '>
+   <section className=' mb-10 mb-5 px-18 '>
      {/* bootcamp */}
   
  <div className=' md:mt-10 '>
   
-   <button className='shadow-lg Buttonn text-black gap-2  text-center  mx-auto flex flex-row mt-10 '>
+   <button className='shadow-lg Buttonn text-black gap-2  text-center  mx-auto flex flex-row  '>
    <Link to ='/register' className={`${styles.flexCenter} mx-auto neue-medium text-[24px] text-center  flex flex-row cursor-pointer p-6`}>Join the Online Bootcamp !
    <img src={arrowhite} alt="" className='w-[48px] mx-2 mt-2  mb-3'/>
    </Link>
